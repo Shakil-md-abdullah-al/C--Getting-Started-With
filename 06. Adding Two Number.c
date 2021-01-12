d88cdd74271788a237dd74b318187c533d0de414
Md. Abdulllah Al Sahkil <65440571+Shakil-md-abdullah-al@users.noreply.github.com>
2021-01-12 21:22:12 +0600

    Adding Two Number

---
+++ 06. Adding Two Number.c
@@ -0,0 +1,17 @@
+//Program to adding two number
+#include<stdio.h>
+
+int main()
+{
+    int a,b,c;
+
+    printf("Enter First Number: ");
+    scanf("%d",&a);
+
+    printf("\nEnter Second Number: ");
+    scanf("%d",&b);
+
+    c=a+b;
+
+    printf("\nThe Summation of %d and %d is: %d\n",a,b,c);
+}
