6b45c8439b6c88966e232b6c03107436370ded8e
Md. Abdulllah Al Sahkil <65440571+Shakil-md-abdullah-al@users.noreply.github.com>
2021-01-12 21:23:47 +0600

    Substract of two number

---
+++ 07. Substract of two number.c
@@ -0,0 +1,18 @@
+//Program to substract two integer
+#include<stdio.h>
+
+int main()
+{
+    int a,b,c;
+
+    printf("Enter First Number: ");
+    scanf("%d",&a);
+
+    printf("\nEnter Second Number: ");
+    scanf("%d",&b);
+
+    c=a-b;
+
+    printf("\nThe Summation of %d and %d is: %d\n",a,b,c);
+}
+
