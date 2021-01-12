5d3be03ae91f97255b80c4f4123127a3d34110ac
Md. Abdulllah Al Sahkil <65440571+Shakil-md-abdullah-al@users.noreply.github.com>
2021-01-12 21:46:27 +0600

    Assigning variable

---
+++ 10. Assigning variable.c
@@ -0,0 +1,14 @@
+//Assining value in variable
+#include<stdio.h>
+
+int main()
+{
+    int a,b,c;
+    printf("Assign the value of a,b,c and get the output.\n");
+
+    a=10;
+    b=20;
+    c=30;
+
+    printf("Print Assigning Value: of a, b,c is : \n%d\t%d\t%d\n",a,b,c);
+}
