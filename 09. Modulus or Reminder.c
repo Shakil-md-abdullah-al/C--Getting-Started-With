0b51d685e6bc8180e62a9b0a52ca1d729d120c07
Md. Abdulllah Al Sahkil <65440571+Shakil-md-abdullah-al@users.noreply.github.com>
2021-01-12 21:27:17 +0600

    Modulus or Reminder

---
+++ 09. Modulus or Reminder.c
@@ -0,0 +1,20 @@
+//Modulus of a division
+#include<stdio.h>
+
+int main()
+{
+    int a,b,c;
+
+    printf("Enter First Number: ");
+    scanf("%d",&a);
+
+    printf("\nEnter Second Number: ");
+    scanf("%d",&b);
+
+    c=a%b;
+
+    printf("\nThe Summation of %d and %d is: %d\n",a,b,c);
+}
+
+
+
